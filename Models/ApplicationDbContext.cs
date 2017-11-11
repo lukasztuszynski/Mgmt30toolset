a3b7532ffbc580abdd646ac4979fcf4dcaf7875e
Łukasz Tuszyński <5594406+lukasztuszynski@users.noreply.github.com>
2017-11-11 21:44:16 +0100

    adds bonuses to application db context

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -7,9 +7,11 @@ namespace Mgmt30toolset.Models
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
-        
+
         public DbSet<Kudo> Kudos { get; set; }
         public DbSet<KudoCategory> KudoCategories { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Bonus> Bonuses { get; set; }
+        public DbSet<BonusTag> BonusTags { get; set; }
     }
 }
